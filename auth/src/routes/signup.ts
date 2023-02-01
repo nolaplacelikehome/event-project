f926f32fd6c648afbf649e3e6390de822b12db3c
jakedwng <jakedwng@gmail.com>
2023-01-31 22:23:35 -0500

    Signup validation and sanitization.
    Also some error handling.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,31 @@
 import express from 'express';
+import { validationResult, body } from 'express-validator'
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-	res.send('Current users...');
+router.post('/api/users/signup', [
+	body('email')
+		.isEmail()
+		.withMessage('Not a valid email.'),
+	body('password')
+		.trim()
+		.isLength({ min: 5, max: 20 })
+		.withMessage('Please enter a valid password')
+],
+	(req: express.Request, res: express.Response) => {
+		const { email, password } = req.body;
+
+		const errors = validationResult(req);
+
+		if(!errors.isEmpty()) {
+			return res.status(400).send(errors.array());
+		}
+		
+		console.log('Creating a user...');
+
+		res.send({});
+	
+
 });
 
 export { router as signUpUser };
